c1ce0435711a527f316a0aeff228afda138d9dd4
lwkchan <laurachan205@gmail.com>
2018-06-26 20:02:34 +0100

    Configure Cypress for first test

---
+++ src/test/integration/todolist.test.js
@@ -0,0 +1,20 @@
+describe('TodoList App', ()=>{
+
+  const appLink = 'http://localhost:3000';
+
+  it('Should load with the correct title', () => {
+    cy.visit(appLink);
+    cy.get('title')
+      .should('contain', 'Todo List App');
+  });
+
+  it('Should allow me to create a todo', () => {
+    const todoText = 'Get better at testing';
+    cy.visit(appLink);
+    cy.get('.todo-input').type(todoText);
+    cy.get('.todo-submit').click();
+    
+    cy.get('.todo-text')
+      .should('equal', todoText);
+  })
+})
