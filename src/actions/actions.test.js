fbc95f39143af3d37080685b7b304e00c1b8f031
lwkchan <laurachan205@gmail.com>
2018-07-01 13:15:42 +0100

    Create action creator

---
+++ src/actions/actions.test.js
@@ -0,0 +1,16 @@
+import actions from './actions'
+import types from '../constants'
+
+describe('Actions', () => {
+  const todoText = 'what i need to do';
+
+  it('should create an action to add a todo', () => {
+    const expectedAction = {
+      type: types.SUBMIT_TODO,
+      id: 1,
+      text: todoText,
+    };
+
+    expect(actions.submitTodo(todoText)).toEqual(expectedAction)
+  })
+})
